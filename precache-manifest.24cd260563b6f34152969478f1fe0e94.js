2f74da67713d7eee02c69b9dd4dcf79a19daa583
JavierAle <javierale25@live.com>
2020-08-31 13:21:12 -0300

    Updates

---
+++ precache-manifest.24cd260563b6f34152969478f1fe0e94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e64a8c3fd11f462213df1b4f1905ea",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "fb8b443e3ff655841d43",
+    "url": "/rick-morty/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5f21e745ad1828a67a1c",
+    "url": "/rick-morty/static/css/main.c5911d0b.chunk.css"
+  },
+  {
+    "revision": "fb8b443e3ff655841d43",
+    "url": "/rick-morty/static/js/2.a48e76b1.chunk.js"
+  },
+  {
+    "revision": "51b6540b076ab0ef424f1d2d2d0c5642",
+    "url": "/rick-morty/static/js/2.a48e76b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f21e745ad1828a67a1c",
+    "url": "/rick-morty/static/js/main.ea986f60.chunk.js"
+  },
+  {
+    "revision": "031736f702c37821150d",
+    "url": "/rick-morty/static/js/runtime-main.00f9065e.js"
+  }
+]);
